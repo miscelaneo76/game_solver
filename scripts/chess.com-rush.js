f0d107ff6e50c126d2dbbbe9bf83dc9ed544b274
Miscelaneo76 <miscelaneo76@gmail.com>
2025-04-16 22:40:07 -0400

    Fix error on chess rush when there is no prev challenge

--- scripts/chess.com-rush.js
+++ scripts/chess.com-rush.js
@@ -1,5 +1,7 @@
 inputManager.type='rush';
 const challenge = JSON.parse(localStorage.json_settings).rushChallenges?.at(0);
-inputManager.index = challenge.puzzleIndex || 0;
-inputManager.addPuzzles(challenge.puzzles || []);
+if(challenge){
+    inputManager.index = challenge.puzzleIndex || 0;
+    inputManager.addPuzzles(challenge.puzzles || []);
+}
 game_solver.style.display = 'block';

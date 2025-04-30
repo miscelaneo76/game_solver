c404bfc8c9289a17eaa7e8f44f1e4b38bcfdbeea
Miscelaneo76 <miscelaneo76@gmail.com>
2025-04-29 22:18:42 -0400

    add rush puzzles when it is required

--- scripts/chess.com-rush.js
+++ scripts/chess.com-rush.js
@@ -1,7 +1,2 @@
 inputManager.type='rush';
-const challenge = JSON.parse(localStorage.json_settings).rushChallenges?.at(-1);
-if(challenge){
-    inputManager.index = challenge.puzzleIndex || 0;
-    inputManager.addPuzzles(challenge.puzzles || []);
-}
 gsInit();
